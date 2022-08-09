b287c8832d5a186bbec74f53932a63fb123a95b2
Oleh <olh.masiuk@gmail.com>
2022-08-09 21:06:08 +0300

    new buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained">Hello World</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+      <Button variant="outlined" color="error">
+        Error
+      </Button>
+
+      <Stack direction="row" spacing={2}>
+        <Button variant="outlined" startIcon={<link href="__"></link>}>
+          Delete
+        </Button>
+        <Button variant="contained" endIcon={<link href="__"></link>}>
+          Send
+        </Button>
+      </Stack>
     </div>
   );
 }
