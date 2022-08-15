af8d5e8ffbd3db89838edde1c22c13a467baeb8b
Oleh <olh.masiuk@gmail.com>
2022-08-15 22:53:50 +0300

    base changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import React from 'react';
-import './App.css';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import React, { createElement } from 'react';
+//our components
+import RequestList from './RequestListComponent/RequestList';
+import SignIn from './SignInComponent/SignIn';
+import SignUp from './SignUpComponent/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <br></br>
-
-      <Button color="secondary">Secondary</Button>
-      <Button variant="contained" color="success">
-        Success test commit
-      </Button>
-      <Button variant="outlined" color="error">
-        Error
-      </Button>
-
+    <div>
+      {/* <SignIn></SignIn>
       <br></br>
       <br></br>
-
-      <Button variant="outlined" startIcon={<link href="__"></link>}>
-        Delete
-      </Button>
-      <Button variant="contained" endIcon={<link href="__"></link>}>
-        Send
-      </Button>
+      <SignUp></SignUp> */}
+      <RequestList></RequestList>
     </div>
   );
+  // return createElement('div', { className: 'container' }, [
+  //   createElement('h1', { className: 'font-bold', key: 1 }, 'Test my JSX'),
+  //   createElement('button', { className: '', key: 2 }, 'Click me'),
+  // ]);
 }
 
 export default App;
