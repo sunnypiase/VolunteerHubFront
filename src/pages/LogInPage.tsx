dd260b2dd9e6ca8f11f22eb6c8aca353d0d1d539
Misha <misha.pavlenko.1111@gmail.com>
2022-09-03 18:53:39 +0300

    VHVar added to login and register

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import SignIn from '../Components/SignIn';
+import VHBar from '../Components/VHBar';
 
 function LogInPage() {
   return (
     <div>
+      <VHBar/>
       <SignIn />
     </div>
   );
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import SignUp from '../Components/SignUp';
+import VHBar from '../Components/VHBar';
 
 function RegisterPage() {
   return (
     <div>
+      <VHBar/>
       <SignUp />
     </div>
   );
