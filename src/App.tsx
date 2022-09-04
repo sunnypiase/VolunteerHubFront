4a37c9f12e4d957f66d107698e8dd3f878ac5335
Ivan Lutsenko <evanlt187@gmail.com>
2022-09-04 11:54:11 +0300

    Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { createTheme } from '@mui/material';
-import ThemeProvider from '@mui/material/styles/ThemeProvider';
-import { Route, Routes } from 'react-router-dom';
-import VHBar from './Components/VHBar';
-import AccountPostsMessages from './pages/AccountMessagesPage';
-import AccountPostsPage from './pages/AccountPostsPage';
-import AccountProfilePage from './pages/AccountProfilePage';
-import CreatePostPage from './pages/CreatePostPage';
-import EditProfilePage from './pages/EditProfilePage';
-import HomePage from './pages/HomePage';
-import LogInPage from './pages/LogInPage';
-import RegisterPage from './pages/RegisterPage';
-import SendPostPage from './pages/SendPostPage';
-import TestPage from './pages/TestPage';
+import { createTheme } from "@mui/material";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import { Route, Routes } from "react-router-dom";
+import VHBar from "./Components/VHBar";
+import AccountPostsMessages from "./pages/AccountMessagesPage";
+import AccountPostsPage from "./pages/AccountPostsPage";
+import AccountProfilePage from "./pages/AccountProfilePage";
+import CreatePostPage from "./pages/CreatePostPage";
+import EditProfilePage from "./pages/EditProfilePage";
+import HomePage from "./pages/HomePage";
+import LogInPage from "./pages/LogInPage";
+import RegisterPage from "./pages/RegisterPage";
+import SendPostPage from "./pages/SendPostPage";
+import TestPage from "./pages/TestPage";
 
 //Need this for proper work with TypeScript
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface Theme {
     status: {
       danger: string;
@@ -31,44 +31,44 @@ declare module '@mui/material/styles' {
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#00ADB5',
-      contrastText: '#FFFFFF',
+      main: "#00ADB5",
+      contrastText: "#FFFFFF",
     },
     secondary: {
-      main: '#000000',
-      contrastText: '#00ADB5',
+      main: "#000000",
+      contrastText: "#00ADB5",
     },
   },
   components: {
     MuiLink: {
       styleOverrides: {
         root: {
-          textDecoration: 'none',
+          textDecoration: "none",
         },
       },
     },
     MuiOutlinedInput: {
       styleOverrides: {
         notchedOutline: {
-          border: 'none',
+          border: "none",
         },
       },
     },
     MuiFormLabel: {
       styleOverrides: {
         root: {
-          fontFamily: 'Inter',
-          fontStyle: 'normal',
-          fontWeight: '300',
-          fontSize: '18px',
+          fontFamily: "Inter",
+          fontStyle: "normal",
+          fontWeight: "300",
+          fontSize: "18px",
         },
       },
     },
     MuiMenu: {
       styleOverrides: {
         list: {
-          backgroundColor: 'rgba(243, 189, 149, 0.72)!important',
-          color: 'a18570',
+          backgroundColor: "rgba(243, 189, 149, 0.72)!important",
+          color: "a18570",
         },
       },
     },
@@ -87,7 +87,7 @@ function App() {
           <Route path="/account/posts" element={<AccountPostsPage />} />
           <Route path="/account/profile" element={<AccountProfilePage />} />
           <Route path="/create-post" element={<CreatePostPage />} />
-          <Route path="/edit" element={<EditProfilePage />} />
+          <Route path="/account/edit" element={<EditProfilePage />} />
           <Route path="/send-post" element={<SendPostPage />} />
           <Route path="/account/messages" element={<AccountPostsMessages />} />
         </Routes>
